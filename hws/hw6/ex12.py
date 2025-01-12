5b7afb95ccf7705ba2cea37be841fd0c1fa0887c
Рауан <kaz.rauan@gmail.com>
2025-01-12 11:09:35 +0500

    Delete hws/hw6 directory
    delete hw6

--- hws/hw6/ex12.py
+++
@@ -1,5 +0,0 @@
-string = input("введи слова: ")
-
-new_string = string[2:6]
-
-print (new_string)
--- hws/hw6/ex9.py
+++
@@ -1,8 +0,0 @@
-a = int(input("enter numb: "))
-
-if a % 2 == 0:
-    result = "чётное"
-else:
-    result = "не чётное"
-
-print (result)
